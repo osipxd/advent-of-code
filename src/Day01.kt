3c0567b76fd4c1419c2460f147fb4ba18d16642b
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-01 20:41:57 +0300

    Day1: Fast solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -8,11 +8,39 @@ fun main() {
     fun part1(elves: Sequence<Sequence<Int>>): Int = elves.maxOf { it.sum() }
 
     // Time - O(N*log(N)), Space - O(N)
-    fun part2(elves: Sequence<Sequence<Int>>): Int {
+    fun part2Simple(elves: Sequence<Sequence<Int>>): Int {
         return elves.map { it.sum() }
             .sortedDescending()
             .take(3)
             .sum()
+    }
+
+    // Time - O(N), Space - O(1)
+    fun part2Fast(elves: Sequence<Sequence<Int>>): Int {
+        val topThree = arrayOf(0, 0, 0)
+
+        for (elf in elves) {
+            val sum = elf.sum()
+
+            when {
+                sum > topThree[0] -> {
+                    topThree[2] = topThree[1]
+                    topThree[1] = topThree[0]
+                    topThree[0] = sum
+                }
+
+                sum > topThree[1] -> {
+                    topThree[2] = topThree[1]
+                    topThree[1] = sum
+                }
+
+                sum > topThree[2] -> {
+                    topThree[2] = sum
+                }
+            }
+        }
+
+        return topThree.sum()
     }
 
     val testInput = readInput("Day01_test")
@@ -20,5 +48,6 @@ fun main() {
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+    println(part2Simple(input))
+    println(part2Fast(input))
 }
