6387cb69cdbc961a3f31b637be1ba3fc466a654b
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-08 10:07:51 +0300

    Day8: Refactored solution

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,73 +1,67 @@
+typealias Forest = List<List<Int>>
+
 fun main() {
     val testInput = readInput("Day08_test")
     val input = readInput("Day08")
 
-    check(part1(testInput) == 21)
-    println("Part 1: " + part1(input))
+    println("=== Part 1 ===")
+    part1(testInput).let {
+        println("Test: $it")
+        check(it == 21)
+    }
+    println("Answer: " + part1(input))
 
-    check(part2(testInput) == 8)
-    println("Part 2: " + part2(input))
+    println("\n=== Part 2 ===")
+    part2(testInput).let {
+        println("Test: $it")
+        check(it == 8)
+    }
+    println("Answer: " + part2(input))
 }
 
-private fun part1(input: List<String>): Int {
-    val n = input.size
-    val m = input[0].length
-    var count = (n + m - 2) * 2
+private fun part1(forest: Forest): Int {
+    val n = forest.size
+    val m = forest[0].size
 
-    fun visibleFrom(r: Int, c: Int, dr: Int, dc: Int): Boolean {
-        val value = input[r][c].digitToInt()
-        var curR = r + dr
-        var curC = c + dc
-        while (curR in input.indices && curC in input[0].indices) {
-            if (input[curR][curC].digitToInt() >= value) return false
-            curR += dr
-            curC += dc
-        }
-
-        return true
+    /** Checks the tree at position [r],[c] is visible from the given [direction]. */
+    fun isVisibleFromOutside(r: Int, c: Int, direction: Direction): Boolean {
+        val treeHeight = forest[r][c]
+        return forest.treesByDirection(r, c, direction).all { it < treeHeight }
     }
 
-    val seen = mutableSetOf<Pair<Int, Int>>()
+    // All trees on edges are visible
+    var count = (n + m - 2) * 2
+
     for (r in 1 until n - 1) {
         for (c in 1 until m - 1) {
-            if (visibleFrom(r, c, 0, +1) ||
-                visibleFrom(r, c, +1, 0) ||
-                visibleFrom(r, c, 0, -1) ||
-                visibleFrom(r, c, -1, 0)
-            ) {
-                if (seen.add(r to c)) count += 1
-            }
+            if (Direction.values().any { isVisibleFromOutside(r, c, it) }) count++
         }
     }
 
     return count
 }
 
-private fun part2(input: List<String>): Int {
-    val n = input.size
-    val m = input[0].length
+private fun part2(forest: Forest): Int {
+    val n = forest.size
+    val m = forest[0].size
+
+    /** Counts trees visible from tree at position [r],[c] in the given [direction]. */
+    fun countVisibleTrees(r: Int, c: Int, direction: Direction): Int {
+        val targetTree = forest[r][c]
+        var count = 0
+        for (currentTree in forest.treesByDirection(r, c, direction)) {
+            count++
+            if (currentTree >= targetTree) return count
+        }
+        return count
+    }
 
     var maxScore = 0
 
-    fun countTrees(r: Int, c: Int, dr: Int, dc: Int): Int {
-        val value = input[r][c].digitToInt()
-        var curR = r + dr
-        var curC = c + dc
-        var score = 0
-        while (curR in input.indices && curC in input[0].indices) {
-            if (input[curR][curC].digitToInt() >= value) return score + 1
-            curR += dr
-            curC += dc
-            score += 1
-        }
-
-        return score
-    }
-
+    // All trees on edges will have score 0, so skip it
     for (r in 1 until n - 1) {
         for (c in 1 until m - 1) {
-            val score =
-                countTrees(r, c, 0, +1) * countTrees(r, c, +1, 0) * countTrees(r, c, 0, -1) * countTrees(r, c, -1, 0)
+            val score = Direction.values().map { countVisibleTrees(r, c, it) }.reduce(Int::times)
             maxScore = maxOf(maxScore, score)
         }
     }
@@ -75,4 +69,25 @@ private fun part2(input: List<String>): Int {
     return maxScore
 }
 
-private fun readInput(name: String) = readLines(name)
+/** Generates sequence of trees heights for the given [direction] in order of distance from [r],[c]. */
+private fun Forest.treesByDirection(r: Int, c: Int, direction: Direction): Sequence<Int> {
+    return generateSequence(direction.next(r to c), direction::next)
+        .takeWhile { (r, c) -> r in indices && c in first().indices }
+        .map { (r, c) -> this[r][c] }
+}
+
+// dr, dc - delta row, delta column
+private enum class Direction(val dr: Int = 0, val dc: Int = 0) {
+    UP(dr = -1),
+    LEFT(dc = -1),
+    DOWN(dr = +1),
+    RIGHT(dc = +1);
+
+    /** Returns the next coordinates on this direction. */
+    fun next(coordinates: Pair<Int, Int>): Pair<Int, Int> {
+        val (r, c) = coordinates
+        return (r + dr) to (c + dc)
+    }
+}
+
+private fun readInput(name: String) = readLines(name).map { it.map(Char::digitToInt) }
