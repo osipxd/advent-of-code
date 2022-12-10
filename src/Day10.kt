219e9218ec2cae15023b74b3e5cde64f7abbe788
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-10 09:43:14 +0300

    Day10: Refactored solution

--- src/Day10.kt
+++ src/Day10.kt
@@ -2,60 +2,44 @@ fun main() {
     val testInput = readInput("Day10_test")
     val input = readInput("Day10")
 
-    "Part 1" {
-        part1(testInput) shouldBe 13140
-        answer(part1(input))
-    }
-
-    "Part 2" {
-        part2(testInput)
+    "Part 1 + 2" {
         println()
-        part2(input)
+        solve(testInput) shouldBe 13140
+        println()
+        answer(solve(input))
     }
 }
 
-private fun part1(input: List<String>): Int {
+private const val START_OFFSET = 20
+private const val WIDTH = 40
+
+private fun solve(commands: List<List<String>>): Int {
     var cycle = 0
     var register = 1
     var signalStrength = 0
 
-    fun tick(registerChange: Int = 0) {
+    fun tick(argument: Int = 0) {
+        // Start new cycle
         cycle++
-        if ((cycle + 20) % 40 == 0) signalStrength += cycle * register
-        register += registerChange
+
+        // Part 1
+        if ((cycle + START_OFFSET) % WIDTH == 0) signalStrength += cycle * register
+
+        // Part 2
+        val isFilled = (cycle - 1) % WIDTH in register - 1..register + 1
+        print(if (isFilled) "#" else ".")
+        if (cycle % WIDTH == 0) println()
+
+        // Finish cycle
+        register += argument
     }
 
-    for (command in input) {
+    for (parts in commands) {
         tick()
-        if (command.startsWith("addx")) tick(command.split(" ").last().toInt())
+        if (parts[0] == "addx") tick(parts[1].toInt())
     }
 
     return signalStrength
 }
 
-private const val ANSI_RESET = "\u001B[0m"
-private const val ANSI_WHITE_BG = "\u001B[47m"
-private const val ANSI_GREY_BG = "\u001B[40m"
-
-// This part has a bug!
-private fun part2(input: List<String>) {
-    var cycle = 0
-    var register = 1
-
-    fun tick(registerChange: Int = 0) {
-        cycle++
-
-        val drawable = register .. register + 2
-        print(if ((cycle % 40) in drawable) "$ANSI_WHITE_BG#$ANSI_RESET" else "$ANSI_GREY_BG.$ANSI_RESET")
-        if (cycle % 40 == 0) println()
-
-        register += registerChange
-    }
-
-    for (command in input) {
-        tick()
-        if (command.startsWith("addx")) tick(command.split(" ").last().toInt())
-    }
-}
-
-private fun readInput(name: String) = readLines(name)
+private fun readInput(name: String) = readLines(name).map { it.split(" ") }
