362b2f46ebc5cfcf5ffc4ba6ebf38e7dac8b3bd0
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-11 10:44:54 +0300

    Day11: Refactored solution

--- src/Day11.kt
+++ src/Day11.kt
@@ -14,78 +14,60 @@ fun main() {
     }
 }
 
-private fun part1(input: List<Monkey>): Int {
-    val inspectionsCounts = IntArray(input.size) { 0 }
+private fun part1(input: List<Monkey>): Long = solve(input, rounds = 20, keepCalm = true)
+private fun part2(input: List<Monkey>): Long = solve(input, rounds = 10_000, keepCalm = false)
 
-    repeat(20) {
-        for ((i, monkey) in input.withIndex()) {
+private fun solve(monkeys: List<Monkey>, rounds: Int, keepCalm: Boolean): Long {
+    val inspectionsCounts = IntArray(monkeys.size) { 0 }
+
+    // All `divideBy` values are prime numbers, so we can multiply them all together
+    // and take remainder from division by resulting value.
+    // We should do it after each operation to not overflow Int.
+    val mod = monkeys.map { it.divideBy.toLong() }.reduce(Long::times)
+
+    repeat(rounds) {
+        for ((i, monkey) in monkeys.withIndex()) {
             inspectionsCounts[i] += monkey.items.size
+
             for (item in monkey.takeItems()) {
-                val new = monkey.operation(item.toLong()) / 3
-                val target = if (new % monkey.testValue == 0L) monkey.ifTrue else monkey.ifFalse
-                input[target].addItem(new.toInt())
+                val calmLevel = if (keepCalm) 3 else 1
+                val new = (monkey.operation(item) / calmLevel % mod).toInt()
+                val target = if (new % monkey.divideBy == 0) monkey.ifTrue else monkey.ifFalse
+                monkeys[target].items.add(new)
             }
         }
     }
 
-    return inspectionsCounts.sortedDescending().take(2).reduce(Int::times)
-}
-
-private fun part2(input: List<Monkey>): Long {
-    val inspectionsCounts = LongArray(input.size) { 0 }
-    val mod = input.map { it.testValue.toLong() }.reduce(Long::times)
-
-    repeat(10000) { round ->
-        for ((i, monkey) in input.withIndex()) {
-            inspectionsCounts[i] += monkey.items.size.toLong()
-            for (item in monkey.takeItems()) {
-                val new = (monkey.operation(item.toLong()) % mod).toInt()
-                val target = if (new % monkey.testValue == 0) monkey.ifTrue else monkey.ifFalse
-                input[target].addItem(new)
-            }
-        }
-        if (round + 1 in listOf(1, 20, 1000, 2000)) {
-            println(inspectionsCounts.contentToString())
-        }
-    }
-
-    return inspectionsCounts.sortedDescending().take(2).reduce(Long::times)
+    return inspectionsCounts.sortedDescending().take(2).map(Int::toLong).reduce(Long::times)
 }
 
 private fun readInput(name: String) = readText(name).splitToSequence("\n\n").map { rawMonkey ->
-    val (items, operation, test, ifTrue, ifFalse) = rawMonkey.split("\n").drop(1)
+    val (items, operation, test, ifTrue, ifFalse) = rawMonkey.lines().drop(1)
 
     Monkey(
-        startingItems = items.substringAfter(": ").split(", ").map { it.toInt() },
-        operation = operationOf(operation),
-        testValue = test.substringAfter("divisible by ").toInt(),
+        startingItems = items.substringAfter(": ").split(", ").map(String::toInt),
+        operation = parseOperation(operation),
+        divideBy = test.substringAfter("divisible by ").toInt(),
         ifFalse = ifFalse.substringAfter("throw to monkey ").toInt(),
         ifTrue = ifTrue.substringAfter("throw to monkey ").toInt(),
     )
 }.toList()
 
-private fun operationOf(operation: String): (Long) -> Long {
-    val parts = operation.substringAfter(" = ").split(" ")
-    val (takeOld, b) = if (parts[2] == "old") true to 0L else false to parts[2].toLong()
-    return if (parts[1] == "+") {
-        { old: Long -> old + if (takeOld) old else b }
-    } else {
-        { old: Long -> old * if (takeOld) old else b }
-    }
+private fun parseOperation(input: String): (Int) -> Long {
+    val parts = input.substringAfter(" = ").split(" ")
+    val operation: (Long, Long) -> Long = if (parts[1] == "+") Long::plus else Long::times
+    val b = parts[2].toLongOrNull()
+    return { old -> operation(old.toLong(), b ?: old.toLong()) }
 }
 
 private class Monkey(
     startingItems: List<Int>,
-    val operation: (Long) -> Long,
-    val testValue: Int,
+    val operation: (Int) -> Long,
+    val divideBy: Int,
     val ifFalse: Int,
     val ifTrue: Int
 ) {
     val items: MutableList<Int> = startingItems.toMutableList()
 
     fun takeItems() = items.toList().also { items.clear() }
-
-    fun addItem(item: Int) {
-        items += item
-    }
 }
