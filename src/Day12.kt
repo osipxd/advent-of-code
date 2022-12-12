c9f6a98b7902c23d864efa15cdc23ae0cfcb49fc
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-12 11:56:59 +0300

    Day12: Refactored solution

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,5 +1,3 @@
-import java.util.*
-
 fun main() {
     "Part 1" {
         val testInput = readInput("Day12_test")
@@ -16,88 +14,72 @@ fun main() {
     }
 }
 
-private fun part1(input: Array<CharArray>): Int {
-    val n = input.size
-    val m = input[0].size
+private fun part1(field: Array<CharArray>): Int = climbDown(field)
+private fun part2(field: Array<CharArray>): Int = climbDown(field, shouldStop = { it == 'a' })
 
-    val (startR, startC) = input.findChar('S')
-    input[startR][startC] = 'a'
-    val (endR, endC) = input.findChar('E')
-    input[endR][endC] = 'z'
+// Time - O(N*M), Space - O(N*M)
+//   where, N - number of rows, M - number of columns
+private fun climbDown(field: Array<CharArray>, shouldStop: (Char) -> Boolean = { false }): Int {
+    // Remember start and end position and then replace it with 'a' and 'z'
+    val (startR, startC) = field.findPositionOf('S')
+    field[startR][startC] = 'a'
 
-    val distance = mutableMapOf((startR to startC) to 0)
+    val (endR, endC) = field.findPositionOf('E')
+    field[endR][endC] = 'z'
 
-    val stack = LinkedList<Pair<Int, Int>>()
-    stack += startR to startC
-
-    fun neighbors(r: Int, c: Int) = sequence {
-        if (r > 0) yield(r - 1 to c)
-        if (r < n - 1) yield(r + 1 to c)
-        if (c > 0) yield(r to c - 1)
-        if (c < m - 1) yield(r to c + 1)
-    }
-
-    while (stack.isNotEmpty()) {
-        val (r, c) = stack.removeFirst()
-        val char = input[r][c]
-
-        if (r == endR && c == endC) break
-
-        for ((r2, c2) in neighbors(r, c)) {
-            if (r2 to c2 in distance || input[r2][c2] > char + 1) continue
-            distance[r2 to c2] = distance.getValue(r to c) + 1
-            stack.addLast(r2 to c2)
-        }
-    }
-
-    return distance.getValue(endR to endC)
-}
-
-private fun part2(input: Array<CharArray>): Int {
-    val n = input.size
-    val m = input[0].size
-
-    val (startR, startC) = input.findChar('S')
-    input[startR][startC] = 'a'
-    val (endR, endC) = input.findChar('E')
-    input[endR][endC] = 'z'
-
+    // Record shortest distance to each
     val distance = mutableMapOf((endR to endC) to 0)
 
-    val stack = LinkedList<Pair<Int, Int>>()
-    stack += endR to endC
+    // Here we will place blocks which we climbed down to.
+    // Yes, we climb down instead of climb up - it is more suitable for Part 2 solution
+    val queue = ArrayDeque<Pair<Int, Int>>()
+    queue += endR to endC
 
-    fun neighbors(r: Int, c: Int) = sequence {
-        if (r > 0) yield(r - 1 to c)
-        if (r < n - 1) yield(r + 1 to c)
-        if (c > 0) yield(r to c - 1)
-        if (c < m - 1) yield(r to c + 1)
-    }
+    // And then, for each block we found, search the next block possible to step down
+    while (queue.isNotEmpty()) {
+        val (r, c) = queue.removeFirst()
 
-    while (stack.isNotEmpty()) {
-        val (r, c) = stack.removeFirst()
-        val char = input[r][c]
+        // We found the shortest path to the start point. Stop
+        if (r == startR && c == startC) break
 
-        if (char == 'a') return distance.getValue(r to c)
+        // Check if we should stop earlier
+        val char = field[r][c]
+        if (shouldStop(char)) return distance.getValue(r to c)
 
-        for ((r2, c2) in neighbors(r, c)) {
-            if (r2 to c2 in distance || char > input[r2][c2] + 1) continue
-            distance[r2 to c2] = distance.getValue(r to c) + 1
-            stack.addLast(r2 to c2)
+        // For each neighbor
+        for (neighbor in field.neighbors(r, c)) {
+            // Check if we can climb down to the neighbor block
+            //  or, if this neighbor was not already checked before
+            val (neighborR, neighborC) = neighbor
+            if (char > field[neighborR][neighborC] + 1 || neighbor in distance) continue
+
+            // Record distance to this neighbor
+            distance[neighbor] = distance.getValue(r to c) + 1
+            // and add it to the queue to check its neighbors
+            queue.addLast(neighbor)
         }
     }
 
-    return distance.getValue(endR to endC)
+    return distance.getValue(startR to startC)
 }
 
-private fun Array<CharArray>.findChar(char: Char): Pair<Int, Int> {
+// Search the given char position for O(N*M) time
+private fun Array<CharArray>.findPositionOf(char: Char): Pair<Int, Int> {
     for (r in indices) {
-        for (c in this[0].indices) {
+        for (c in first().indices) {
             if (this[r][c] == char) return r to c
         }
     }
 
-    error("")
+    error("Given char not found!")
+}
+
+/** Creates sequence of neighbors around the given [r], [c] */
+private fun Array<CharArray>.neighbors(r: Int, c: Int) = sequence {
+    if (r > 0) yield(r - 1 to c)
+    if (r < lastIndex) yield(r + 1 to c)
+    if (c > 0) yield(r to c - 1)
+    if (c < first().lastIndex) yield(r to c + 1)
 }
 
 private fun readInput(name: String) = readLines(name).map { it.toCharArray() }.toTypedArray()
