42d4719ce3b44f21f5fd14f4a3e60e4f7bbf2901
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-13 20:43:18 +0300

    offtop: Add template

---
+++ src/DayX.kt
@@ -0,0 +1,21 @@
+fun main() {
+    val day = "14"
+    val testInput = readInput("Day${day}_test")
+    val input = readInput("Day$day")
+
+    "Part 1" {
+        part1(testInput) shouldBe 0
+        answer(part1(input))
+    }
+
+    //"Part 2" {
+    //    part2(testInput) shouldBe 0
+    //    answer(part2(input))
+    //}
+}
+
+private fun part1(input: List<String>): Int = TODO()
+
+private fun part2(input: List<String>): Int = TODO()
+
+private fun readInput(name: String) = readLines(name)
