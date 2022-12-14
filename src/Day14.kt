21b0562a6253aedac59fac1d1c941e3ae2f68b74
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-14 10:31:27 +0300

    Day14: Refactored solution

--- src/Day14.kt
+++ src/Day14.kt
@@ -13,122 +13,84 @@ fun main() {
     }
 }
 
-private const val SHIFT = 0
-private const val SIZE = 1000
+private fun part1(input: List<List<List<Int>>>) = simulateSandFalling(input, hasFloor = false)
+private fun part2(input: List<List<List<Int>>>) = simulateSandFalling(input, hasFloor = true)
 
-private fun part1(input: List<List<List<Int>>>): Int {
-    val map = Array(SIZE) { Array(SIZE) { '.' } }
+private const val AIR = '.'
+private const val ROCK = '#'
+private const val SAND = 'o'
 
+private const val SAND_SOURCE_COL = 500
+
+private const val WIDTH = SAND_SOURCE_COL * 2
+private const val HEIGHT = WIDTH / 2
+
+private fun simulateSandFalling(structures: List<List<List<Int>>>, hasFloor: Boolean): Int {
+    val map = Array(HEIGHT) { Array(WIDTH) { AIR } }
+
+    // Add rock structures from the input to the map
     var lastRow = 0
-    for (points in input) {
-        points
-            .onEach { lastRow = maxOf(lastRow, it[1]) }
+    for (structure in structures) {
+        structure
+            // Remember last row, it will be helpful later
+            .onEach { lastRow = maxOf(lastRow, it.row) }
             .windowed(2) { (from, to) ->
-                for (c in (from[0]..to[0]).normalize()) {
-                    for (r in (from[1]..to[1]).normalize()) {
-                        map[r][c - SHIFT] = '#'
+                // Either row, or column will be equal, so we just draw a line, not a square
+                for (row in (from.row..to.row).fixOrder()) {
+                    for (col in (from.col..to.col).fixOrder()) {
+                        map[row][col] = ROCK
                     }
                 }
             }
     }
 
-    var fallen = 0
-    val (fallR, fallC) = 0 to (500 - SHIFT)
+    // Add floor if we need it
+    if (hasFloor) {
+        lastRow += 2
+        for (col in 0 until WIDTH) map[lastRow][col] = ROCK
+    }
 
+    // Here we will count how many sand blocks have fallen already
+    var sandBlocks = 0
+
+    // Simulate sand falling one-by-one
     while (true) {
-        fallen++
-        var col = fallC
-        var row = fallR
+        var row = 0
+        var col = SAND_SOURCE_COL
+
+        // Can we spawn new sand block?
+        if (map[row][col] != AIR) break
+
         while (row < lastRow) {
             when {
-                map[row + 1][col] == '.' -> {
-                    row++
-                }
+                map[row + 1][col] == AIR -> Unit      // no-op
+                map[row + 1][col - 1] == AIR -> col-- // move left
+                map[row + 1][col + 1] == AIR -> col++ // move right
 
-                map[row + 1][col - 1] == '.' -> {
-                    row++
-                    col--
-                }
-
-                map[row + 1][col + 1] == '.' -> {
-                    row++
-                    col++
-                }
-
+                // It is impossible to fall below, so just place sand block here and stop falling
                 else -> {
-                    map[row][col] = 'o'
+                    sandBlocks++
+                    map[row][col] = SAND
                     break
                 }
             }
+
+            row++
         }
 
         if (row >= lastRow) break
     }
 
-//    println(map.joinToString("\n") { it.joinToString("") })
-    return fallen - 1
+    return sandBlocks
 }
 
-private fun part2(input: List<List<List<Int>>>): Int {
-    val map = Array(SIZE) { Array(SIZE) { '.' } }
+/** Returns [this] range in right order. */
+private fun IntRange.fixOrder() = if (start > endInclusive) endInclusive..start else this
 
-    var lastRow = 0
-    for (points in input) {
-        points
-            .onEach { lastRow = maxOf(lastRow, it[1]) }
-            .windowed(2) { (from, to) ->
-                for (c in (from[0]..to[0]).normalize()) {
-                    for (r in (from[1]..to[1]).normalize()) {
-                        map[r][c - SHIFT] = '#'
-                    }
-                }
-            }
-    }
+// "498,4 -> 498,6 -> 496,6" => [[498, 4], [498, 6], [496, 6]]
+private fun readInput(name: String) =
+    readLines(name).map { line -> line.split(" -> ").map { it.splitInts(",") } }
 
-    for (c in 0 until SIZE) map[lastRow + 2][c] = '#'
-    lastRow += 2
-
-    var fallen = 0
-    val (fallR, fallC) = 0 to (500 - SHIFT)
-
-    while (true) {
-        fallen++
-        if (map[fallR][fallC] != '.') break
-
-        var col = fallC
-        var row = fallR
-        while (row < lastRow) {
-            when {
-                map[row + 1][col] == '.' -> {
-                    row++
-                }
-
-                map[row + 1][col - 1] == '.' -> {
-                    row++
-                    col--
-                }
-
-                map[row + 1][col + 1] == '.' -> {
-                    row++
-                    col++
-                }
-
-                else -> {
-                    map[row][col] = 'o'
-                    break
-                }
-            }
-        }
-
-        if (row >= lastRow) break
-    }
-
-//    println(map.joinToString("\n") { it.joinToString("") })
-    return fallen - 1
-}
-
-private fun IntRange.normalize() = if (start > endInclusive) endInclusive..start else this
-
-private fun readInput(name: String) = readLines(name).map { line ->
-    line.split(" -> ").map { it.split(",").map(String::toInt) }
-}
+// Extensions to hide 0 and 1 indices from code
+private val List<Int>.col: Int get() = get(0)
+private val List<Int>.row: Int get() = get(1)
