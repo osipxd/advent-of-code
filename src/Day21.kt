a087638dd43d5aa26402a7d25da0a6dff7488a15
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-21 11:37:48 +0300

    Day21: Part 2 solution

--- src/Day21.kt
+++ src/Day21.kt
@@ -15,11 +15,43 @@ fun main() {
 
 private fun part1(input: Map<String, YellingMonkey>): Long {
     input.calculateMonkeyAnswer("root")
-    return input.getValue("root").answer!!
+    return input.requireAnswer("root").toLong()
 }
 
 private fun part2(input: Map<String, YellingMonkey>): Long {
-    TODO()
+    val rootMonkey = input.getValue("root")
+    val me = input.getValue("humn")
+
+    // Calculate answers for left and right side
+    input.calculateMonkeyAnswer("root")
+    val (leftBefore, rightBefore) = rootMonkey.waitFor.map(input::requireAnswer)
+    input.resetAnswers()
+
+    // Increase my answer by 1 to see how thi will affect resulting answer
+    val originalAnswer = me.answer!!
+    me.answer = originalAnswer + 1
+
+    // Calculate answers for left and right side again to figure out
+    // what part has changed and how it is changed
+    input.calculateMonkeyAnswer("root")
+    val (leftAfter, rightAfter) = rootMonkey.waitFor.map(input::requireAnswer)
+
+    // Calculate value shift - divide original gap by te diff after we changed answer by one
+    val shift = if (rightAfter == rightBefore) {
+        (leftBefore - rightBefore) / (leftBefore - leftAfter)
+    } else {
+        (rightBefore - leftBefore) / (rightBefore - rightAfter)
+    }
+
+    return (originalAnswer + shift).toLong()
+}
+
+private fun Map<String, YellingMonkey>.requireAnswer(name: String) = checkNotNull(getValue(name).answer)
+
+private fun Map<String, YellingMonkey>.resetAnswers() {
+    values.asSequence()
+        .filter { it.waitFor.isNotEmpty() }
+        .forEach { it.answer = null }
 }
 
 private fun Map<String, YellingMonkey>.calculateMonkeyAnswer(monkey: String) {
@@ -49,12 +81,12 @@ private fun Map<String, YellingMonkey>.calculateMonkeyAnswer(monkey: String) {
 private fun readInput(name: String) = readLines(name).map { line ->
     val (monkeyName, rawOperation) = line.split(": ")
     val operationParts = rawOperation.split(" ")
-    val operation: ((Long, Long) -> Long)? = if (operationParts.size == 3) {
+    val operation: ((Double, Double) -> Double)? = if (operationParts.size == 3) {
         when (operationParts[1]) {
-            "+" -> Long::plus
-            "*" -> Long::times
-            "/" -> Long::div
-            "-" -> Long::minus
+            "+" -> Double::plus
+            "*" -> Double::times
+            "/" -> Double::div
+            "-" -> Double::minus
             else -> error("Unknown operation ${operationParts[1]}")
         }
     } else null
@@ -62,16 +94,14 @@ private fun readInput(name: String) = readLines(name).map { line ->
     YellingMonkey(
         name = monkeyName,
         waitFor = if (operationParts.size == 3) listOf(operationParts[0], operationParts[2]) else emptyList(),
-        symbol = operationParts.getOrNull(1).orEmpty(),
         operation = operation,
-    ).apply { answer = rawOperation.toLongOrNull() }
+    ).apply { answer = rawOperation.toDoubleOrNull() }
 }.associateBy { it.name }
 
 private data class YellingMonkey(
     val name: String,
     val waitFor: List<String>,
-    val symbol: String,
-    val operation: ((Long, Long) -> Long)?,
+    val operation: ((Double, Double) -> Double)?,
 ) {
-    var answer: Long? = null
+    var answer: Double? = null
 }
