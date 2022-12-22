fd31ad32a820ab0fe537409c6ce2042297b14e1b
Osip Fatkullin <osip.fatkullin@gmail.com>
2022-12-22 11:09:14 +0300

    Day22: Horrible part 2

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,16 +1,18 @@
+import CubeSide.*
+
 fun main() {
     val (testMap, testPath) = readInput("Day22_test")
     val (map, path) = readInput("Day22")
 
-    "Part 1" {
-        part1(testMap, testPath) shouldBe 6032
-        measureAnswer { part1(map, path) }
-    }
+//    "Part 1" {
+//        part1(testMap, testPath) shouldBe 6032
+//        measureAnswer { part1(map, path) }
+//    }
 
-    //"Part 2" {
-    //    part2(testInput) shouldBe 5031
-    //    measureAnswer { part2(input) }
-    //}
+    "Part 2" {
+//        part2(testMap, testPath) shouldBe 5031
+        measureAnswer { part2(map, path) }
+    }
 }
 
 private fun part1(map: Map, path: List<Step>): Int {
@@ -23,8 +25,14 @@ private fun part1(map: Map, path: List<Step>): Int {
     return map.password()
 }
 
-private fun part2(input: List<String>): Int {
-    TODO()
+private fun part2(map: Map, path: List<Step>): Int {
+    map.reset()
+    path.forEach { step ->
+        println(step)
+        map.applyStep(step)
+    }
+
+    return map.password()
 }
 
 private fun readInput(name: String): Pair<Map, List<Step>> {
@@ -56,19 +64,60 @@ private fun readInput(name: String): Pair<Map, List<Step>> {
 private class Map(
     val internal: List<String>
 ) {
-    val height = internal.size
-    val width = internal[0].length
 
-    var row = 0
-    var col = 0
-    var direction = 0
+    private val cubeSideNeighbors = mapOf(
+        (TOP to DIRECTION_RIGHT) to (RIGHT to DIRECTION_RIGHT),
+        (TOP to DIRECTION_DOWN) to (FRONT to DIRECTION_DOWN),
+        (TOP to DIRECTION_LEFT) to (LEFT to DIRECTION_RIGHT),
+        (TOP to DIRECTION_UP) to (BACK to DIRECTION_RIGHT),
 
-    init { reset() }
+        (RIGHT to DIRECTION_RIGHT) to (BOTTOM to DIRECTION_LEFT),
+        (RIGHT to DIRECTION_DOWN) to (FRONT to DIRECTION_LEFT),
+        (RIGHT to DIRECTION_LEFT) to (TOP to DIRECTION_LEFT),
+        (RIGHT to DIRECTION_UP) to (BACK to DIRECTION_UP),
+
+        (FRONT to DIRECTION_RIGHT) to (RIGHT to DIRECTION_UP),
+        (FRONT to DIRECTION_DOWN) to (BOTTOM to DIRECTION_DOWN),
+        (FRONT to DIRECTION_LEFT) to (LEFT to DIRECTION_DOWN),
+        (FRONT to DIRECTION_UP) to (TOP to DIRECTION_UP),
+
+        (LEFT to DIRECTION_RIGHT) to (BOTTOM to DIRECTION_RIGHT),
+        (LEFT to DIRECTION_DOWN) to (BACK to DIRECTION_DOWN),
+        (LEFT to DIRECTION_LEFT) to (TOP to DIRECTION_RIGHT),
+        (LEFT to DIRECTION_UP) to (FRONT to DIRECTION_RIGHT),
+
+        (BOTTOM to DIRECTION_RIGHT) to (RIGHT to DIRECTION_LEFT),
+        (BOTTOM to DIRECTION_DOWN) to (BACK to DIRECTION_LEFT),
+        (BOTTOM to DIRECTION_LEFT) to (LEFT to DIRECTION_LEFT),
+        (BOTTOM to DIRECTION_UP) to (FRONT to DIRECTION_UP),
+
+        (BACK to DIRECTION_RIGHT) to (BOTTOM to DIRECTION_UP),
+        (BACK to DIRECTION_DOWN) to (RIGHT to DIRECTION_DOWN),
+        (BACK to DIRECTION_LEFT) to (TOP to DIRECTION_DOWN),
+        (BACK to DIRECTION_UP) to (LEFT to DIRECTION_UP),
+    )
+
+    val cubeSize = internal.size / 4
+    val cubeLastIndex = cubeSize - 1
+    val cubeIndices = 0..cubeLastIndex
+
+    val row get() = row(cubeRow)
+    val col get() = col(cubeCol)
+
+    var cubeRow = 0
+    var cubeCol = 0
+    var cubeSide = TOP
+    var direction = DIRECTION_RIGHT
+
+    init {
+        reset()
+    }
 
     fun reset() {
-        row = 0
-        col = internal[0].indexOfFirst { it == AIR }
-        direction = 0
+        cubeRow = 0
+        cubeCol = 0
+        cubeSide = TOP
+        direction = DIRECTION_RIGHT
     }
 
     fun password() = 1000 * (row + 1) + 4 * (col + 1) + direction
@@ -85,41 +134,147 @@ private class Map(
     }
 
     private fun goForward(distance: Int) {
-        println("$distance from $row, $col")
+        println("from ${row + 1}, ${col + 1} forward $distance in $direction")
         for (i in 1..distance) {
-            val (r, c) = nextInDirection()
-            if (internal[r][c] == AIR) {
-                row = r
-                col = c
-            } else {
-                break
-            }
+            if (!stepInDirection()) break
         }
     }
 
-    private fun nextInDirection(): Pair<Int, Int> {
+    private fun cubeSide(r: Int, c: Int): Int {
+        return when {
+            r < cubeSize -> 1
+            r in cubeSize until (cubeSize * 2) -> 1 + c / cubeSize
+            else -> 4 + (c - (cubeSize * 2)) / cubeSize
+        }
+    }
+
+    private fun stepInDirection(): Boolean {
         val (dr, dc) = directions[direction]
 
-        var nextRow = row
-        var nextCol = col
-
-        if (dc != 0) {
-            do {
-                nextCol = (nextCol + dc).mod(internal[row].length)
-            } while (internal[row][nextCol] == VOID)
-        } else {
-            do {
-                nextRow = (nextRow + dr).mod(internal.size)
-            } while (col > internal[nextRow].lastIndex || internal[nextRow][col] == VOID)
+        var nextRow = cubeRow + dr
+        var nextCol = cubeCol + dc
+        if (nextRow in cubeIndices && nextCol in cubeIndices) {
+            println("$cubeSide $nextRow,$nextCol")
+            println("$cubeSide ${row(nextRow)},${col(nextCol)} (${internal.size}x${internal[row(nextRow)].length})")
+            return if (internal[row(nextRow)][col(nextCol)] == AIR) {
+                cubeRow = nextRow
+                cubeCol = nextCol
+                true
+            } else {
+                false
+            }
         }
 
-        return nextRow to nextCol
+        val (newSide, newDirection) = cubeSideNeighbors.getValue(cubeSide to direction)
+        when (direction) {
+            newDirection -> {
+                nextRow = nextRow.mod(cubeSize)
+                nextCol = nextCol.mod(cubeSize)
+            }
+
+            DIRECTION_RIGHT -> when (newDirection) {
+                DIRECTION_LEFT -> {
+                    nextRow = cubeLastIndex - cubeRow
+                    nextCol = cubeLastIndex
+                }
+
+                DIRECTION_DOWN -> {
+                    nextRow = 0
+                    nextCol = cubeLastIndex - cubeRow
+                }
+
+                DIRECTION_UP -> {
+                    nextRow = cubeLastIndex
+                    nextCol = cubeRow
+                }
+
+                else -> error("Unexpected move $cubeSide -> $newSide")
+            }
+
+            DIRECTION_DOWN -> when (newDirection) {
+                DIRECTION_UP -> {
+                    nextRow = cubeLastIndex
+                    nextCol = cubeLastIndex - cubeCol
+                }
+
+                DIRECTION_RIGHT -> {
+                    nextRow = cubeLastIndex - cubeCol
+                    nextCol = 0
+                }
+
+                DIRECTION_LEFT -> {
+                    nextRow = cubeCol
+                    nextCol = cubeLastIndex
+                }
+
+                else -> error("Unexpected move $cubeSide -> $newSide")
+            }
+
+            DIRECTION_LEFT -> when (newDirection) {
+                DIRECTION_DOWN -> {
+                    nextRow = 0
+                    nextCol = cubeRow
+                }
+
+                DIRECTION_UP -> {
+                    nextRow = cubeLastIndex
+                    nextCol = cubeLastIndex - cubeRow
+                }
+
+                DIRECTION_RIGHT -> {
+                    nextRow = cubeLastIndex - cubeRow
+                    nextCol = 0
+                }
+
+                else -> error("Unexpected move $cubeSide -> $newSide")
+            }
+
+            DIRECTION_UP -> when (newDirection) {
+                DIRECTION_DOWN -> {
+                    nextRow = 0
+                    nextCol = cubeLastIndex - cubeCol
+                }
+
+                DIRECTION_RIGHT -> {
+                    nextRow = cubeCol
+                    nextCol = 0
+                }
+
+                DIRECTION_LEFT -> {
+                    nextRow = cubeLastIndex - cubeCol
+                    nextCol = cubeLastIndex
+                }
+
+                else -> error("Unexpected move $cubeSide -> $newSide")
+            }
+        }
+
+        println(internal[row(nextRow, newSide)].length)
+        return if (internal[row(nextRow, newSide)][col(nextCol, newSide)] == AIR) {
+            println("$cubeRow,$cubeCol ($cubeSide) -> $nextRow,$nextCol ($newSide)")
+
+            cubeRow = nextRow
+            cubeCol = nextCol
+            cubeSide = newSide
+            direction = newDirection
+            true
+        } else {
+            false
+        }
     }
+
+    private fun row(cubeRow: Int, side: CubeSide = cubeSide) = side.row * cubeSize + cubeRow
+    private fun col(cubeCol: Int, side: CubeSide = cubeSide) = side.col * cubeSize + cubeCol
 
     companion object {
         const val AIR = '.'
         const val WALL = '#'
         const val VOID = ' '
+
+        const val DIRECTION_RIGHT = 0
+        const val DIRECTION_DOWN = 1
+        const val DIRECTION_LEFT = 2
+        const val DIRECTION_UP = 3
 
         val directions = listOf(
             0 to +1,
@@ -128,6 +283,15 @@ private class Map(
             -1 to 0,
         )
     }
+}
+
+private enum class CubeSide(val row: Int, val col: Int) {
+    TOP(row = 0, col = 1),
+    RIGHT(row = 0, col = 2),
+    FRONT(row = 1, col = 1),
+    LEFT(row = 2, col = 0),
+    BOTTOM(row = 2, col = 1),
+    BACK(row = 3, col = 0),
 }
 
 private sealed interface Step {
