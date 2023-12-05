b726b2d9b2350d1e524355a24b6dd6d24978803a
Osip Fatkullin <osip.fatkullin@gmail.com>
2023-12-06 00:34:55 +0100

    Day5: Looks better but still inefficient

--- advent-of-code-2023/src/Day5.kt
+++ advent-of-code-2023/src/Day5.kt
@@ -17,82 +17,58 @@ fun main() {
 
 // Runtime: 10ms
 private fun part1(almanac: Almanac): Long {
-    return almanac.seeds.minOf { seed -> almanac.findLocation(seed) }
+    return almanac.seeds.minOf { seed ->
+        var result = seed
+        for (mappings in almanac.mappingsLayers) result = mappings.map(result)
+        result
+    }
 }
+
+private fun List<Mapping>.map(value: Long) = find { value in it }?.map(value) ?: value
 
 // Runtime: 4m 35s
 private fun part2(almanac: Almanac): Long {
     return almanac.seeds.chunked(2).minOf { (rangeStart, rangeSize) ->
-        (rangeStart until (rangeStart + rangeSize))
-            .also { println(it) }
-            .minOf { seed -> almanac.findLocation(seed) }
+        (rangeStart rangeOfSize rangeSize).minOf(almanac::findLocation)
     }
 }
 
 private fun readInput(name: String): Almanac {
-    val input = ArrayDeque(readLines(name))
-    fun readMappings(): List<Mapping> {
-        if (input.first().isEmpty()) input.removeFirst()
-        input.removeFirst() // Drop description
-        return buildList {
-            while (input.isNotEmpty() && input.first().isNotEmpty()) {
-                val (destinationStart, sourceStart, rangeSize) = input.removeFirst().split(" ").map(String::toLong)
-                add(
-                    Mapping(
-                        range = sourceStart until (sourceStart + rangeSize),
-                        offset = destinationStart - sourceStart,
-                    )
-                )
-            }
-        }
+    val almanacInput = readText(name).split("\n\n")
+
+    fun parseMapping(line: String): Mapping {
+        val (destinationStart, sourceStart, rangeSize) = line.splitLongs()
+        return Mapping(
+            range = sourceStart rangeOfSize rangeSize,
+            offset = destinationStart - sourceStart,
+        )
     }
 
     return Almanac(
-        seeds = input.removeFirst().substringAfter(": ").split(" ").map(String::toLong),
-        seedToSoil = readMappings(),
-        soilToFertilizer = readMappings(),
-        fertilizerToWater = readMappings(),
-        waterToLight = readMappings(),
-        lightToTemperature = readMappings(),
-        temperatureToHumidity = readMappings(),
-        humidityToLocation = readMappings(),
+        seeds = almanacInput.first().substringAfter(": ").splitLongs(),
+        mappingsLayers = almanacInput.drop(1).map { mappingInput ->
+            mappingInput.lineSequence()
+                .drop(1) // Skip mappings layer description
+                .map(::parseMapping)
+                .toList()
+        }
     )
 }
 
 private data class Almanac(
     val seeds: List<Long>,
-    val seedToSoil: List<Mapping>,
-    val soilToFertilizer: List<Mapping>,
-    val fertilizerToWater: List<Mapping>,
-    val waterToLight: List<Mapping>,
-    val lightToTemperature: List<Mapping>,
-    val temperatureToHumidity: List<Mapping>,
-    val humidityToLocation: List<Mapping>,
-) {
-    fun findLocation(seed: Long): Long {
-        return seed
-            .let(seedToSoil::map)
-            .let(soilToFertilizer::map)
-            .let(fertilizerToWater::map)
-            .let(waterToLight::map)
-            .let(lightToTemperature::map)
-            .let(temperatureToHumidity::map)
-            .let(humidityToLocation::map)
-    }
-}
-
-private fun List<Mapping>.map(value: Long): Long {
-    val mapping = find { value in it } ?: Mapping.OneToOne
-    return mapping.map(value)
-}
+    val mappingsLayers: List<List<Mapping>>
+)
 
 private class Mapping(val range: LongRange, val offset: Long) {
 
     operator fun contains(value: Long) = value in range
 
     fun map(value: Long) = value + offset
-
-    companion object {
-        val OneToOne = Mapping(0..Long.MAX_VALUE, 0)
-    }
 }
+
+// region Utils
+private fun String.splitLongs() = split(" ").map(String::toLong)
+
+private infix fun Long.rangeOfSize(size: Long) = this..<(this + size)
+// endregion
