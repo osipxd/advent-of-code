fdd00ba378ebe39651deec18eaff4c2593cccd02
Osip Fatkullin <osip.fatkullin@gmail.com>
2023-12-07 00:28:26 +0100

    Day5: Optimize ranges check

--- advent-of-code-2023/src/Day5.kt
+++ advent-of-code-2023/src/Day5.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 private const val DAY = "Day5"
 
 fun main() {
@@ -15,22 +18,47 @@ fun main() {
     }
 }
 
-// Runtime: 10ms
 private fun part1(almanac: Almanac): Long {
     return almanac.seeds.minOf { seed ->
         var result = seed
-        for (mappings in almanac.mappingsLayers) result = mappings.map(result)
+        for (mappings in almanac.mappingsLayers) result = mappings.transform(result)
         result
     }
 }
 
-private fun List<Mapping>.map(value: Long) = find { value in it }?.map(value) ?: value
+private fun List<Mapping>.transform(value: Long) = find { value in it }?.transform(value) ?: value
 
-// Runtime: 4m 35s
 private fun part2(almanac: Almanac): Long {
-    return almanac.seeds.chunked(2).minOf { (rangeStart, rangeSize) ->
-        (rangeStart rangeOfSize rangeSize).minOf(almanac::findLocation)
+    // Initially fill with ranges from input
+    // Pairs: <layer of mapping> to <range>
+    val ranges = ArrayDeque(
+        almanac.seeds.chunked(2)
+            .map { (start, size) -> 0 to (start rangeOfSize size) }
+    )
+
+    val lastLayer = almanac.mappingsLayers.lastIndex
+    var minLocation = Long.MAX_VALUE
+
+    while (ranges.isNotEmpty()) {
+        val (layer, range) = ranges.removeFirst()
+        val mapping = almanac.mappingsLayers[layer].find { range in it }
+        val rangeForNextLayer = if (mapping != null) {
+            val (before, intersection, after) = range intersect mapping.range
+            if (before != null) ranges.addFirst(layer to before)
+            if (after != null) ranges.addFirst(layer to after)
+            mapping.transform(intersection)
+        } else {
+            range
+        }
+
+        if (layer == lastLayer) {
+            minLocation = min(minLocation, rangeForNextLayer.first)
+        } else {
+            ranges.addLast(layer + 1 to rangeForNextLayer)
+        }
     }
+
+    return minLocation
 }
 
 private fun readInput(name: String): Almanac {
@@ -63,12 +91,21 @@ private data class Almanac(
 private class Mapping(val range: LongRange, val offset: Long) {
 
     operator fun contains(value: Long) = value in range
+    operator fun contains(values: LongRange) = values.first in range || values.last in range
 
-    fun map(value: Long) = value + offset
+    fun transform(value: Long) = value + offset
+    fun transform(values: LongRange) = transform(values.first)..transform(values.last)
 }
 
 // region Utils
 private fun String.splitLongs() = split(" ").map(String::toLong)
 
 private infix fun Long.rangeOfSize(size: Long) = this..<(this + size)
+
+// Returns: (before?, intersection, after?)
+private infix fun LongRange.intersect(other: LongRange) = Triple(
+    (first..<max(first, other.first)).takeUnless { it.isEmpty() }, // Before
+    max(first, other.first)..min(last, other.last), // Intersection
+    (min(last, other.last) + 1..last).takeUnless { it.isEmpty() }, // After
+)
 // endregion
