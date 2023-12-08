5052af3a697337dcfaab52e5933665c008cf4add
Osip Fatkullin <osip.fatkullin@gmail.com>
2023-12-08 13:32:32 +0100

    Day8: Refactor solution

--- advent-of-code-2023/src/Day8.kt
+++ advent-of-code-2023/src/Day8.kt
@@ -18,45 +18,20 @@ fun main() {
     }
 }
 
-private fun part1(input: DessertMap): Int {
-    var steps = 0
-    var currentNode = "AAA"
-    while (currentNode != "ZZZ") {
-        steps++
-        val node = input.nodes.getValue(currentNode)
-        currentNode = when (input.getInstruction(steps)) {
-            'L' -> node.ifLeft
-            'R' -> node.ifRight
-            else -> error("Unexpected instruction")
-        }
-    }
-
-    return steps
-}
+private fun part1(input: DessertMap): Int = input.countSteps(fromNode = "AAA") { it == "ZZZ" }
 
 private fun part2(input: DessertMap): Long {
-    val startNodes = input.nodes.keys.filter { it.endsWith("A") }
-    val cycles = startNodes.map { startNode ->
-        var currentNode = startNode
-        var steps = 0L
-
-        while (!currentNode.endsWith("Z")) {
-            steps++
-            val node = input.nodes.getValue(currentNode)
-            currentNode = when (input.getInstruction(steps)) {
-                'L' -> node.ifLeft
-                'R' -> node.ifRight
-                else -> error("Unexpected instruction")
-            }
+    val instructionsCount = input.instructions.length
+    // Assumption is that we can reach each end node in a whole number of instructions cycles.
+    // And number of cycles for each node is a prime number.
+    return input.nodes.keys.filter { it.endsWith("A") }
+        .map { startNode ->
+            var stepsUntilEnd = input.countSteps(startNode) { it.endsWith("Z") }
+            // This IF is needed only for test input, where one of paths takes 1.5 cycles
+            if (stepsUntilEnd % instructionsCount != 0) stepsUntilEnd *= 2
+            (stepsUntilEnd / instructionsCount).toLong()
         }
-        steps
-    }
-
-    val maxCycle = cycles.max()
-    var steps = maxCycle
-    while (cycles.any { steps % it != 0L }) steps += maxCycle
-
-    return steps
+        .reduce(Long::times) * instructionsCount
 }
 
 private fun readInput(name: String): DessertMap {
@@ -65,8 +40,8 @@ private fun readInput(name: String): DessertMap {
         instructions = instructions,
         nodes = rawNodes.lineSequence().associate { line ->
             val (nodeId, directions) = line.split(" = ")
-            val (ifLeft, ifRight) = directions.trim('(', ')').split(", ")
-            nodeId to MapNode(ifLeft, ifRight)
+            val (leftNode, rightNode) = directions.trim('(', ')').split(", ")
+            nodeId to MapNode(leftNode, rightNode)
         },
     )
 }
@@ -75,11 +50,26 @@ private data class DessertMap(
     val instructions: String,
     val nodes: Map<String, MapNode>
 ) {
-    fun getInstruction(step: Int) = instructions[(step - 1) % instructions.length]
-    fun getInstruction(step: Long) = instructions[((step - 1) % instructions.length).toInt()]
+
+    fun countSteps(fromNode: String, predicate: (String) -> Boolean): Int {
+        var steps = 0
+        var currentNode = fromNode
+        fun instruction() = instructions[(steps - 1) % instructions.length]
+
+        while (!predicate(currentNode)) {
+            steps++
+            currentNode = nodes.getValue(currentNode).nextNode(instruction())
+        }
+
+        return steps
+    }
 }
 
-private data class MapNode(
-    val ifLeft: String,
-    val ifRight: String,
-)
+private data class MapNode(val leftNode: String, val rightNode: String) {
+
+    fun nextNode(instruction: Char) = when (instruction) {
+        'L' -> leftNode
+        'R' -> rightNode
+        else -> error("Unexpected instruction: $instruction")
+    }
+}
