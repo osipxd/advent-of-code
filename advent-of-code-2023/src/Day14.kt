476d24badb2052b1ee30f33fd2f94ca4e0653d41
Osip Fatkullin <osip.fatkullin@gmail.com>
2023-12-14 23:13:18 +0100

    Day14: Part 2 - Raw solution

--- advent-of-code-2023/src/Day14.kt
+++ advent-of-code-2023/src/Day14.kt
@@ -1,3 +1,5 @@
+private typealias Platform = List<CharArray>
+
 private const val DAY = "Day14"
 
 fun main() {
@@ -15,35 +17,101 @@ fun main() {
     }
 }
 
-private fun part1(input: List<String>): Int = input.columns().sumOf(::calculateWeight)
+private fun part1(input: Platform): Int {
+    input.tilt(TiltDirection.NORTH)
+    return input.calculateNorthWeight()
+}
 
-private fun part2(input: List<String>): Int = TODO()
+private fun part2(input: Platform): Int {
+    val memory = mutableListOf<List<String>>()
+    val cyclesCount = 1_000_000_000
+    for (cycle in 0..<cyclesCount) {
+        input.rotate()
 
-private fun calculateWeight(line: String): Int {
-    var freePosition = 0
-    var weight = 0
-    val maxWeight = line.length
+        val snapshot = input.map(::String)
+        val seenBeforeAt = memory.indexOf(snapshot)
 
-    for ((i, char) in line.withIndex()) {
-        when (char) {
-            'O' -> {
-                weight += maxWeight - freePosition
-                freePosition++
+        if (seenBeforeAt != -1) {
+            val cycleSize = cycle - seenBeforeAt
+            return memory[seenBeforeAt - 1 + (cyclesCount - seenBeforeAt) % cycleSize].map { it.toCharArray() }
+                .calculateNorthWeight()
+        }
+
+        memory += snapshot
+    }
+
+    error("There is no cycle?")
+}
+
+private fun Platform.rotate() {
+    TiltDirection.entries.forEach(::tilt)
+}
+
+private fun Platform.tilt(direction: TiltDirection) {
+    for (mainAxis in indices) {
+        var freePosition = 0
+        for (movingAxis in indices) {
+            val position = direction.derivePosition(mainAxis, movingAxis, lastIndex)
+            when (this[position]) {
+                'O' -> {
+                    val positionToSet = direction.derivePosition(mainAxis, freePosition, lastIndex)
+                    this[position] = '.'
+                    this[positionToSet] = 'O'
+                    freePosition++
+                }
+
+                '#' -> freePosition = movingAxis + 1
             }
+        }
+    }
+}
 
-            '#' -> freePosition = i + 1
-            else -> Unit // Ignore
+private fun Platform.calculateNorthWeight(): Int {
+    val maxWeight = size
+    var weight = 0
+    for (col in indices) {
+        for (row in indices) {
+            if (this[row][col] == 'O') weight += maxWeight - row
         }
     }
 
     return weight
 }
 
-private fun readInput(name: String) = readLines(name)
+private fun readInput(name: String) = readLines(name).map { it.toCharArray() }
+    .also { input -> check(input.size == input.first().size) }
 
-private fun List<String>.columns(): List<String> = buildList {
-    for (i in this@columns.first().indices) {
-        val column = buildString { for (line in this@columns) append(line[i]) }
-        add(column)
+private enum class TiltDirection(val horizontal: Boolean, val forward: Boolean) {
+    NORTH(horizontal = false, forward = true),
+    WEST(horizontal = true, forward = true),
+    SOUTH(horizontal = false, forward = false),
+    EAST(horizontal = true, forward = false);
+
+    val vertical: Boolean = !horizontal
+    val backward: Boolean = !forward
+
+    fun derivePosition(mainAxis: Int, movingAxis: Int, maximum: Int): Pair<Int, Int> {
+        val row = when {
+            vertical && forward -> movingAxis
+            vertical && backward -> maximum - movingAxis
+            else -> mainAxis
+        }
+        val col = when {
+            horizontal && forward -> movingAxis
+            horizontal && backward -> maximum - movingAxis
+            else -> mainAxis
+        }
+
+        return row to col
     }
+}
+
+private operator fun Platform.get(position: Pair<Int, Int>): Char {
+    val (row, col) = position
+    return this[row][col]
+}
+
+private operator fun Platform.set(position: Pair<Int, Int>, value: Char) {
+    val (row, col) = position
+    this[row][col] = value
 }
