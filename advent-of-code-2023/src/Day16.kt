bf52ff2fb687814542760e8f6596718ef5499ac1
Osip Fatkullin <osip.fatkullin@gmail.com>
2023-12-16 12:35:48 +0100

    Day16: Part 1 - Raw solution

---
+++ advent-of-code-2023/src/Day16.kt
@@ -0,0 +1,109 @@
+private const val DAY = "Day16"
+
+fun main() {
+    fun testInput() = readInput("${DAY}_test")
+    fun input() = readInput(DAY)
+
+    "Part 1" {
+        part1(testInput()) shouldBe 46
+        measureAnswer { part1(input()) }
+    }
+
+    //"Part 2" {
+    //    part2(testInput()) shouldBe 0
+    //    measureAnswer { part2(input()) }
+    //}
+}
+
+private fun part1(field: List<String>): Int {
+    val beams = ArrayDeque<Beam>()
+    val seenBeams = mutableSetOf<Beam>()
+
+    fun addBeam(beam: Beam) {
+        if (beam.position in field && beam !in seenBeams) {
+            seenBeams += beam
+            beams.addLast(beam)
+        }
+    }
+
+    addBeam(Beam(DIRECTION_RIGHT, Position(0, 0)))
+    while (beams.isNotEmpty()) {
+        val beam = beams.removeFirst()
+        val (nextBeam, forkedBeam) = beam.move(field[beam.position])
+        addBeam(nextBeam)
+        forkedBeam?.let(::addBeam)
+    }
+
+    return seenBeams.distinctBy { it.position }.size
+}
+
+private fun part2(input: List<String>): Int = TODO()
+
+private data class Beam(
+    val direction: Int,
+    val position: Position,
+) {
+
+    fun move(currentTile: Char): Pair<Beam, Beam?> {
+        val (direction, forkedBeam) = processTile(currentTile)
+        var (row, col) = position
+        when (direction) {
+            DIRECTION_UP -> row--
+            DIRECTION_RIGHT -> col++
+            DIRECTION_DOWN -> row++
+            DIRECTION_LEFT -> col--
+        }
+
+        return Beam(direction, row to col) to forkedBeam
+    }
+
+    private fun processTile(tile: Char): Pair<Int, Beam?> {
+        var forkedBeam: Beam? = null
+        var direction = this.direction
+        when (tile) {
+            in "\\/" -> direction = turn(direction, tile)
+
+            '|' -> if (direction.horizontal) {
+                direction = DIRECTION_UP
+                forkedBeam = copy(direction = DIRECTION_DOWN)
+            }
+
+            '-' -> if (direction.vertical) {
+                direction = DIRECTION_LEFT
+                forkedBeam = copy(direction = DIRECTION_RIGHT)
+            }
+        }
+
+        return direction to forkedBeam
+    }
+}
+
+private fun turn(direction: Int, mirror: Char): Int {
+    val turnClockwise = when (direction) {
+        DIRECTION_UP, DIRECTION_DOWN -> mirror == '/'
+        DIRECTION_RIGHT, DIRECTION_LEFT -> mirror == '\\'
+        else -> error("Unexpected direction: $direction")
+    }
+
+    return (if (turnClockwise) direction + 1 else direction - 1).mod(4)
+}
+
+private fun readInput(name: String) = readLines(name)
+
+private const val DIRECTION_UP = 0
+private const val DIRECTION_RIGHT = 1
+private const val DIRECTION_DOWN = 2
+private const val DIRECTION_LEFT = 3
+
+private val Int.vertical get() = this % 2 == 0
+private val Int.horizontal get() = this % 2 == 1
+
+private operator fun List<String>.get(position: Position): Char {
+    val (row, col) = position
+    return this[row][col]
+}
+
+private operator fun List<String>.contains(position: Position): Boolean {
+    val (row, col) = position
+    return row in indices && col in first().indices
+}
