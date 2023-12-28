96995406b395345710c3bd3a7d2760bbba090eda
Osip Fatkullin <osip.fatkullin@gmail.com>
2023-12-28 23:50:42 +0100

    Day24: Part 1 - Raw solution

--- advent-of-code-2023/src/Day24.kt
+++ advent-of-code-2023/src/Day24.kt
@@ -1,4 +1,5 @@
 import lib.combinations
+import lib.component6
 
 private const val DAY = "Day24"
 
@@ -17,45 +18,53 @@ fun main() {
     //}
 }
 
+// region Part 1
 private fun part1(input: List<Hailstone>, testArea: LongRange): Int {
-    return input.combinations().count { (first, second) -> first.xyCrossedWith(second, testArea) }
+    return input.combinations().count { (first, second) ->
+        val (a1, b1, c1) = first.toLine()
+        val (a2, b2, c2) = second.toLine()
+
+        val k = determinant(a1, a2, b1, b2)
+        if (k == 0.0) return@count false // Parallel vectors
+
+        val px = determinant(c1, c2, b1, b2) / k
+        val py = determinant(a1, a2, c1, c2) / k
+
+        px in testArea && py in testArea &&
+            first.isPointInFuture(px, py) && second.isPointInFuture(px, py)
+    }
 }
+
+private fun Hailstone.toLine() = Line(
+    a = v.y,
+    b = -v.x,
+    c = v.y * p.x - v.x * p.y,
+)
+
+private fun Hailstone.isPointInFuture(px: Double, py: Double) = v.x * (px - p.x) >= 0 && v.y * (py - p.y) >= 0
+
+/** Representing the line in common form: ax + by = c */
+private data class Line(val a: Long, val b: Long, val c: Long)
+
+/** Calculates determinant of the 2x2 matrix containing [a], [b], [c], [d] values. */
+private fun determinant(a: Long, b: Long, c: Long, d: Long): Double {
+    return a.toDouble() * d - b.toDouble() * c
+}
+
+private operator fun LongRange.contains(value: Double): Boolean = value >= first && value <= last
+// endregion
 
 private fun part2(input: List<Hailstone>): Int = TODO()
 
 private fun readInput(name: String) = readLines(name) { line ->
-    val (xyz, vector) = line.split(" @ ")
-    val (x, y, z) = xyz.split(", ").map { it.toLong() }
-    val (dx, dy, dz) = vector.splitInts(", ")
-    Hailstone(x, y, z, dx, dy, dz)
+    val (x, y, z, dx, dy, dz) = line.split(" @ ", ", ").map { it.toLong() }
+    Hailstone(XYZ(x, y, z), XYZ(dx, dy, dz))
 }
 
-private class Hailstone(
-    val x: Long,
-    val y: Long,
-    val z: Long,
-    val dx: Int,
-    val dy: Int,
-    val dz: Int,
-) {
-
-    // Representing the line in common form: ax + by = c
-    val a = dy
-    val b = -dx
-    val c = dy * x - dx * y
-
-    fun xyCrossedWith(other: Hailstone, testRange: LongRange): Boolean {
-        val k = (this.a * other.b - other.a * this.b).toDouble()
-        if (k == 0.0) return false // Parallel vectors
-
-        val px = (this.c * other.b - other.c * this.b) / k
-        val py = (this.a * other.c - other.a * this.c) / k
-
-        return px in testRange && py in testRange &&
-            this.isPointInFuture(px, py) && other.isPointInFuture(px, py)
-    }
-
-    private fun isPointInFuture(px: Double, py: Double) = dx * (px - x) >= 0 && dy * (py - y) >= 0
+private data class XYZ(val x: Long, val y: Long, val z: Long) {
+    override fun toString(): String = "($x, $y, $z)"
 }
 
-private operator fun LongRange.contains(value: Double): Boolean = value >= first && value <= last
+private class Hailstone(val p: XYZ, val v: XYZ) {
+    override fun toString(): String = "$p @ $v"
+}
