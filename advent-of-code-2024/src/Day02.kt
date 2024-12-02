fe97b805eefe06426c07ed11bd175f9c719d6d17
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-12-02 09:47:52 +0100

    Day02: Part 1 - Raw

---
+++ advent-of-code-2024/src/Day02.kt
@@ -0,0 +1,46 @@
+import kotlin.math.abs
+import kotlin.math.sign
+
+private const val DAY = "Day02"
+
+fun main() {
+    fun testInput() = readInput("${DAY}_test")
+    fun input() = readInput(DAY)
+
+    "Part 1" {
+        part1(testInput()) shouldBe 2
+        measureAnswer { part1(input()) }
+    }
+
+    //"Part 2" {
+    //    part2(testInput()) shouldBe 0
+    //    measureAnswer { part2(input()) }
+    //}
+}
+
+private fun part1(input: List<List<Int>>): Int = input.count { it.isSafe() }
+
+private fun part2(input: List<List<Int>>): Int = TODO()
+
+private fun List<Int>.isSafe(): Boolean {
+    var prev = -1
+    var sign = 0
+
+    for (curr in this) {
+        if (prev == -1) {
+            prev = curr
+            continue
+        }
+
+        val diff = curr - prev
+        if (abs(diff) > 3 || diff == 0 || sign != 0 && diff.sign != sign) {
+            return false
+        }
+        prev = curr
+        sign = diff.sign
+    }
+
+    return true
+}
+
+private fun readInput(name: String) = readLines(name) { it.splitInts() }
