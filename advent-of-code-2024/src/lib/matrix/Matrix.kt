3ab36c6f4d0b57411605c67d43348bbd512302f3
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-12-04 10:57:56 +0100

    offtop: Bring matrix utils

---
+++ advent-of-code-2024/src/lib/matrix/Matrix.kt
@@ -0,0 +1,56 @@
+package lib.matrix
+
+import printValue
+import readLines
+
+/**
+ * 2-dimensional matrix.
+ * Assumes that the given [lines] are all equal by length.
+ */
+class Matrix<T>(lines: List<List<T>>) {
+
+    val rowCount: Int = lines.size
+    val lastRowIndex: Int = rowCount - 1
+    val rowIndices: IntRange = 0..lastRowIndex
+
+    val columnCount: Int = lines.first().size
+    val lastColumnIndex: Int = columnCount - 1
+    val columnIndices: IntRange = 0..lastColumnIndex
+
+    private val values: MutableList<T> = MutableList(rowCount * columnCount) { i ->
+        lines[i / columnCount][i % columnCount]
+    }
+
+    fun rows(): List<List<T>> = values.chunked(columnCount)
+
+    fun row(row: Int): List<T> = values.slice(index(row, 0)..index(row, lastColumnIndex))
+
+    fun column(column: Int): List<T> = buildList {
+        for (row in rowIndices) add(this@Matrix[row, column])
+    }
+
+    operator fun get(row: Int, column: Int): T = values[index(row, column)]
+
+    operator fun set(row: Int, column: Int, value: T) {
+        values[index(row, column)] = value
+    }
+
+    private fun index(row: Int, column: Int): Int = row * columnCount + column
+}
+
+val Matrix<*>.topLeftPosition: Position get() = Position.Zero
+val Matrix<*>.bottomRightPosition: Position get() = Position(lastRowIndex, lastColumnIndex)
+
+fun <T> List<List<T>>.toMatrix(): Matrix<T> = Matrix(this)
+
+/** Prints matrix row by row representing each element as a char. */
+fun <T> Matrix<T>.debugPrint(transform: (position: Position, value: T) -> Char) {
+    for (row in rowIndices) {
+        columnIndices.joinToString(separator = "") { col -> transform(Position(row, col), this[row, col]).toString() }
+            .printValue()
+    }
+}
+
+fun readMatrix(fileName: String): Matrix<Char> = readMatrix(fileName) { line -> line.map { it } }
+fun <T> readMatrix(fileName: String, lineElements: (String) -> List<T>): Matrix<T> =
+     readLines(fileName, lineElements).toMatrix()
---
+++ advent-of-code-2024/src/lib/matrix/Position.kt
@@ -0,0 +1,23 @@
+package lib.matrix
+
+/** Data class representing position in Matrix. */
+data class Position(val row: Int, val column: Int) {
+
+    fun offsetBy(row: Int = 0, column: Int = 0): Position = Position(this.row + row, this.column + column)
+
+    override fun toString(): String = "($row, $column)"
+
+    companion object {
+        val Zero: Position = Position(0, 0)
+    }
+}
+
+operator fun Matrix<*>.contains(position: Position): Boolean {
+    return position.row in rowIndices && position.column in columnIndices
+}
+
+operator fun <T> Matrix<T>.get(position: Position): T = this[position.row, position.column]
+
+operator fun <T> Matrix<T>.set(position: Position, value: T) {
+    this[position.row, position.column] = value
+}
