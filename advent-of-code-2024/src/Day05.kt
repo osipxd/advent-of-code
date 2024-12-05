cd15dc54620fb705a192069a975600d04209129d
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-12-05 15:09:41 +0100

    Day05: Clean up solution, use comparator

--- advent-of-code-2024/src/Day05.kt
+++ advent-of-code-2024/src/Day05.kt
@@ -18,53 +18,38 @@ fun main() {
     }
 }
 
-private fun part1(input: Pair<List<Rule>, List<Update>>): Int {
+private fun part1(input: Pair<Set<Rule>, List<Update>>): Int {
     val (rules, updates) = input
-    val followersMap = rules.groupBy { it.first }.mapValues { (_, values) -> values.mapTo(mutableSetOf()) { it.second } }
+    val comparator = rules.comparator()
 
-    return updates.filter { it.isSafe(followersMap) }
+    return updates.filter { it.isSorted(comparator) }
         .sumOf { it[it.size / 2] }
 }
 
-private fun part2(input: Pair<List<Rule>, List<Update>>): Int {
+private fun part2(input: Pair<Set<Rule>, List<Update>>): Int {
     val (rules, updates) = input
-    val followersMap = rules.groupBy { it.first }.mapValues { (_, values) -> values.mapTo(mutableSetOf()) { it.second } }
+    val comparator = rules.comparator()
 
-    return updates.filterNot { it.isSafe(followersMap) }
-        // Don't say anything!
-        .map { it.fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap).fixedOrder(followersMap) }
+    return updates.filterNot { it.isSorted(comparator) }
+        .map { it.sortedWith(comparator) }
         .sumOf { it[it.size / 2] }
 }
 
-private fun Update.isSafe(followersMap: Map<Int, Set<Int>>): Boolean {
-    for (i in indices.reversed()) {
-        val num = get(i)
-        val followers = followersMap[num] ?: continue
-
-        if ((subList(0, i).toSet() intersect followers).isNotEmpty()) return false
-    }
-    return true
+private fun <T> List<T>.isSorted(comparator: Comparator<T>): Boolean {
+    return asSequence().windowed(2).all { (o1, o2) -> comparator.compare(o1, o2) == -1 }
 }
 
-private fun Update.fixedOrder(followersMap: Map<Int, Set<Int>>): Update {
-    val result = ArrayDeque<Int>()
-
-    for (num in this) {
-        val followers = followersMap[num].orEmpty()
-        val firstFollowerIndex = result.indexOfFirst { it in followers }
-        when (firstFollowerIndex) {
-            -1 -> result.addLast(num)
-            0 -> result.addFirst(num)
-            else -> result.add(firstFollowerIndex - 1, num)
-        }
+private fun Set<Rule>.comparator(): Comparator<Int> = Comparator { o1, o2 ->
+    when {
+        (o1 to o2) in this -> -1
+        (o2 to o1) in this -> 1
+        else -> error("Where did you get this input? It's not from Advent of Code :)")
     }
-
-    return result
 }
 
-private fun readInput(name: String): Pair<List<Rule>, List<Update>> {
+private fun readInput(name: String): Pair<Set<Rule>, List<Update>> {
     val (rawRules, rawUpdates) = readText(name).split("\n\n")
-    val rules = rawRules.lines().map { it.splitInts("|").takePair() }
+    val rules = rawRules.lines().map { it.splitInts("|").takePair() }.toSet()
     val updates = rawUpdates.lines().map { it.splitInts() }
 
     return rules to updates
