daa41a7f750878b3882870f5cd7c69199f629d6e
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-12-07 14:10:48 +0100

    Day07: Part 2 - Raw

--- advent-of-code-2024/src/Day07.kt
+++ advent-of-code-2024/src/Day07.kt
@@ -9,29 +9,34 @@ fun main() {
         measureAnswer { part1(input()) }
     }
 
-    //"Part 2" {
-    //    part2(testInput()) shouldBe 0
-    //    measureAnswer { part2(input()) }
-    //}
+    "Part 2" {
+        part2(testInput()) shouldBe 11387
+        measureAnswer { part2(input()) }
+    }
 }
 
 private fun part1(input: List<Equation>): Long =
     input.filter { isPossibleEquation(it.numbers, it.testValue) }
         .sumOf { it.testValue }
 
-private fun isPossibleEquation(numbers: List<Long>, testValue: Long): Boolean {
+private fun part2(input: List<Equation>): Long =
+    input.filter { isPossibleEquation(it.numbers, it.testValue, useConcatenation = true) }
+        .sumOf { it.testValue }
+
+private fun isPossibleEquation(numbers: List<Long>, testValue: Long, useConcatenation: Boolean = false): Boolean {
     fun check(index: Int, value: Long, nextIndex: Int = index + 1): Boolean = when {
         index == numbers.lastIndex -> value == testValue
         value > testValue -> false
 
         else -> check(nextIndex, value + numbers[nextIndex]) ||
-                check(nextIndex, value * numbers[nextIndex])
+            check(nextIndex, value * numbers[nextIndex]) ||
+            useConcatenation && check(nextIndex, value concat numbers[nextIndex])
     }
 
     return check(index = 0, value = numbers.first())
 }
 
-private fun part2(input: List<Equation>): Long = TODO()
+private infix fun Long.concat(other: Long): Long = (this.toString() + other.toString()).toLong()
 
 private fun readInput(name: String) = readLines(name) { line ->
     Equation(
