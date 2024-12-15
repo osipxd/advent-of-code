52dcb9b11db981e50b9ee31e81f4dcb1e10c9c68
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-12-15 14:42:03 +0100

    Day13: Part 2 - Raw

--- advent-of-code-2024/src/Day13.kt
+++ advent-of-code-2024/src/Day13.kt
@@ -1,5 +1,6 @@
+
 import kotlin.math.abs
-import kotlin.math.roundToInt
+import kotlin.math.roundToLong
 
 private const val DAY = "Day13"
 
@@ -12,28 +13,29 @@ fun main() {
         measureAnswer { part1(input()) }
     }
 
-    //"Part 2" {
-    //    part2(testInput()) shouldBe 0
-    //    measureAnswer { part2(input()) }
-    //}
+    "Part 2" {
+        part2(testInput()).printValue()
+        measureAnswer { part2(input()) }
+    }
 }
 
-private const val EPSILON = 1e-6
+private fun part1(input: List<ClawMachine>): Long = solve(input, maxTaps = 100)
+private fun part2(input: List<ClawMachine>): Long = solve(input, targetShift = 10000000000000)
 
-private fun part1(input: List<ClawMachine>): Int {
-    return input.sumOf { clawMachine ->
-        val (a, b) = clawMachine.solve()
-        val (roundedA, roundedB) = listOf(a, b).map { it.roundToInt() }
+private fun solve(machines: List<ClawMachine>, targetShift: Long = 0, maxTaps: Int = -1): Long {
+    return machines.sumOf { clawMachine ->
+        val (a, b) = clawMachine.solve(targetShift)
+        val (roundedA, roundedB) = listOf(a, b).map { it.asLongOrNull() }
 
-        if (a > 100 || b > 100 || abs(a - roundedA) > EPSILON || abs(b - roundedB) > EPSILON) {
+        if (roundedA == null || roundedB == null || maxTaps != -1 && (roundedA > maxTaps || roundedB > maxTaps)) {
             0
         } else {
-            a.roundToInt() * 3 + b.roundToInt() * 1
+            tapsCost(roundedA, roundedB)
         }
     }
 }
 
-private fun part2(input: List<ClawMachine>): Int = TODO()
+private fun tapsCost(aTaps: Long, bTaps: Long) = aTaps * 3 + bTaps * 1
 
 private val TWO_NUMBERS_PATTERN = Regex(""".+?(\d+),.+?(\d+)""")
 
@@ -72,16 +74,28 @@ private data class ClawMachine(
      *    ⎧ a = tx / ax - b * bx / ax
      *    ⎩ b = ty / by - a * ay / by
      *
-     * 3. Use the second equation in the first
+     * 3. Use the second equation in the first in place of `b`
      *    a = tx / ax - (ty / by - a * ay / by) * bx / ax              // next: unwrap brackets
      *    a = tx / ax - ty / by * bx / ax + a * ay / by * bx / ax      // next: move all `a`s to the left part
      *    a - a * ay / by * bx / ax = tx / ax - ty / by * bx / ax      // next: move `a` outside of brackets
      *    a * (1 - ay / by * bx / ax) = tx / ax - ty / by * bx / ax    // next: keep only `a` on the left side
      *    a = (tx / ax - ty / by * bx / ax) / (1 - ay / by * bx / ax)
      */
-    fun solve(): Pair<Double, Double> {
-        val a = (tx.toDouble() / ax - ty.toDouble() / by * bx / ax) / (1 - ay.toDouble() / by * bx / ax)
-        val b = ty.toDouble() / by - a * ay / by
+    fun solve(targetShift: Long): Pair<Double, Double> {
+        val tx = (tx + targetShift).toDouble()
+        val ty = (ty + targetShift).toDouble()
+
+        val a = (tx / ax - ty / by * bx / ax) / (1 - ay.toDouble() / by * bx / ax)
+        val b = ty / by - a * ay / by
         return a to b
     }
+}
+
+// Utils
+
+private const val EPSILON = 1e-3
+
+private fun Double.asLongOrNull(): Long? {
+    val rounded = roundToLong()
+    return if (abs(this - rounded) > EPSILON) null else rounded
 }
