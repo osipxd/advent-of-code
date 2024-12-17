626d14f1b5fc8316e16b91e9d53ef8422a7718de
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-12-17 11:48:44 +0100

    Day17: Part 2 - Raw

--- advent-of-code-2024/src/Day17.kt
+++ advent-of-code-2024/src/Day17.kt
@@ -1,44 +1,65 @@
 private const val DAY = "Day17"
 
 fun main() {
-    fun testInput() = readInput("${DAY}_test")
+    fun testInput(n: Int) = readInput("${DAY}_test$n")
     fun input() = readInput(DAY)
 
     "Part 1" {
-        part1(testInput()) shouldBe "4,6,3,5,6,3,5,2,1,0"
+        part1(testInput(1)) shouldBe "4,6,3,5,6,3,5,2,1,0"
         measureAnswer { part1(input()) }
     }
 
-    //"Part 2" {
-    //    part2(testInput()) shouldBe 0
-    //    measureAnswer { part2(input()) }
-    //}
+    "Part 2" {
+        part2(testInput(2)) shouldBe 117440
+        measureAnswer { part2(input()) }
+    }
 }
 
-private fun part1(computer: Computer): String {
-    computer.runProgram()
-    return computer.out.joinToString(",")
+private fun part1(computer: Computer): String = computer.runProgram().joinToString(",")
+
+private fun part2(computer: Computer): Long {
+    val states = ArrayDeque<Pair<Long, Int>>()
+    fun addStateRange(range: LongRange, elementsCount: Int) = states.addAll(range.map { it to elementsCount })
+
+    addStateRange(1..7L, elementsCount = 1)
+    while (states.isNotEmpty()) {
+        val (registerA, elementsCount) = states.removeFirst()
+        val output = computer.runProgram(registerA)
+        if (output.size == elementsCount && output == computer.program.takeLast(elementsCount)) {
+            if (output.size == computer.program.size) return registerA
+            else addStateRange((registerA * 8)..<((registerA + 1) * 8 - 1), elementsCount + 1)
+        }
+    }
+
+    error("It is not possible for the given program")
 }
 
-private fun part2(computer: Computer): Int = TODO()
-
-private class Computer(val registers: IntArray, val program: List<Int>) {
+private class Computer(val registers: LongArray, val program: List<Int>) {
     var instructionPointer = 0
     val out = mutableListOf<Int>()
 
-    fun comboOperand(operand: Int): Int = when (operand) {
-        in 0..3 -> operand
+    fun comboOperand(operand: Int): Long = when (operand) {
+        in 0..3 -> operand.toLong()
         in 4..6 -> registers[operand - 4]
         else -> error("Invalid combo operand: $operand")
     }
 
-    fun runProgram() {
+    fun runProgram(registerA: Long = registers[A]): List<Int> {
+        reset()
+        registers[A] = registerA
         while (instructionPointer in program.indices) {
             val pointerBefore = instructionPointer
             val instruction = Instruction.byOpcode(program[instructionPointer])
             with(instruction) { execute(program[instructionPointer + 1]) }
             if (pointerBefore == instructionPointer) instructionPointer += 2
         }
+        return out.toList()
+    }
+
+    private fun reset() {
+        instructionPointer = 0
+        out.clear()
+        registers.fill(0)
     }
 }
 
@@ -52,17 +73,17 @@ private fun interface Instruction {
     companion object {
         private val instructionsByOpcode = listOf(
             /* adv */ xdv(A),
-            /* bxl */ Instruction { operand -> registers[B] = registers[B] xor operand },
+            /* bxl */ Instruction { operand -> registers[B] = registers[B] xor operand.toLong() },
             /* bst */ Instruction { operand -> registers[B] = comboOperand(operand) % 8 },
-            /* jnz */ Instruction { operand -> if (registers[A] != 0) instructionPointer = operand },
+            /* jnz */ Instruction { operand -> if (registers[A] != 0L) instructionPointer = operand },
             /* bxc */ Instruction { registers[B] = registers[B] xor registers[C] },
-            /* out */ Instruction { operand -> out.add(comboOperand(operand) % 8) },
+            /* out */ Instruction { operand -> out.add((comboOperand(operand) % 8).toInt()) },
             /* bdv */ xdv(B),
             /* cdv */ xdv(C),
         )
 
         private fun xdv(register: Int) = Instruction { operand ->
-            val denominator = powOf2(comboOperand(operand))
+            val denominator = powOf2(comboOperand(operand).toInt())
             registers[register] = registers[A] / denominator
         }
 
@@ -73,7 +94,7 @@ private fun interface Instruction {
 private fun readInput(name: String): Computer {
     val (rawRegisters, rawProgram) = readText(name).split("\n\n")
     return Computer(
-        registers = rawRegisters.lines().map { it.substringAfter(": ").toInt() }.toIntArray(),
+        registers = rawRegisters.lines().map { it.substringAfter(": ").toLong() }.toLongArray(),
         program = rawProgram.substringAfter(": ").splitInts()
     )
 }
