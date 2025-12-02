32fe0ac3e890740c568232ca0dbf9872847f38ed
Osip Fatkullin <osip.fatkullin@gmail.com>
2025-12-02 11:24:31 +0100

    Day01: Part1 - Raw

---
+++ advent-of-code-2025/src/Day01.kt
@@ -0,0 +1,36 @@
+private const val DAY = "Day01"
+
+fun main() {
+    fun testInput() = readInput("${DAY}_test")
+    fun input() = readInput(DAY)
+
+    "Part 1" {
+        part1(testInput()) shouldBe 3
+        measureAnswer { part1(input()) }
+    }
+
+    //"Part 2" {
+    //    part2(testInput()) shouldBe 0
+    //    measureAnswer { part2(input()) }
+    //}
+}
+
+private const val MAX_POSITION = 99
+
+private fun part1(input: List<Int>): Int {
+    var password = 0
+    input.fold(50) { acc, rotation ->
+        val newPosition = (acc + rotation).mod(MAX_POSITION + 1)
+        if (newPosition == 0) password++
+        newPosition
+    }
+
+    return password
+}
+
+private fun part2(input: List<Int>): Int = TODO()
+
+private fun readInput(name: String) = readLines(name).map { line ->
+    val sign = if (line.first() == 'R') +1 else -1
+    sign * line.drop(1).toInt()
+}
