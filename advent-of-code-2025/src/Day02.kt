a31e51ee32d6de9f6fefd3d651b0aea0237ddb8b
Osip Fatkullin <osip.fatkullin@gmail.com>
2025-12-02 11:14:24 +0100

    Day02: Part2 - Raw

--- advent-of-code-2025/src/Day02.kt
+++ advent-of-code-2025/src/Day02.kt
@@ -9,32 +9,39 @@ fun main() {
         measureAnswer { part1(input()) }
     }
 
-    //"Part 2" {
-    //    part2(testInput()) shouldBe 0
-    //    measureAnswer { part2(input()) }
-    //}
+    "Part 2" {
+        part2(testInput()) shouldBe 4174379265
+        measureAnswer { part2(input()) }
+    }
 }
 
-private fun part1(input: List<LongRange>): Long = input.sumOf { range ->
-    val startLength = range.first.length
-    val endLength = range.last.length
+private fun part1(input: List<LongRange>): Long = input.sumOf { it.invalidIds(repetitions = 2).sum() }
 
-    val firstHalfStart = range.first.take(startLength / 2)
-    val firstHalfEnd = range.last.take((endLength + 1) / 2)
-
-    (firstHalfStart..firstHalfEnd).asSequence()
-        .map { "$it$it".toLong() }
-        .filter { it in range }
-        .sum()
+private fun part2(input: List<LongRange>): Long = input.sumOf { range ->
+    val lengths = range.first.length..range.last.length
+    val possibleRepetitions = (2..lengths.last).asSequence()
+        .filter { repetitions -> lengths.any { it % repetitions == 0 } }
+    possibleRepetitions.flatMap { range.invalidIds(it) }.distinct().sum()
 }
 
-private fun part2(input: List<IntRange>): Int = TODO()
+private fun LongRange.invalidIds(repetitions: Int): Sequence<Long> {
+    check(repetitions >= 2) { "Invalid repetitions: $repetitions. Should be >= 2" }
+
+    val firstPartStart = first.take { length -> length / repetitions }
+    val firstPartEnd = last.take { length -> (length + repetitions - 1) / repetitions }
+
+    return (firstPartStart..firstPartEnd).asSequence()
+        .map { "$it".repeat(repetitions).toLong() }
+        .filter { it in this }
+}
 
 private val Long.length get() = toString().length
 
 // Example: 1234.take(2) -> 12
-private fun Long.take(count: Int) = toString().take(count).ifEmpty { "0" }.toLong()
-
+private inline fun Long.take(count: (length: Int) -> Int): Long {
+    val stringValue = toString()
+    return stringValue.take(count(stringValue.length)).ifEmpty { "0" }.toLong()
+}
 
 private fun readInput(name: String) = readText(name).split(",").map {
     val (start, end) = it.splitLongs("-")
