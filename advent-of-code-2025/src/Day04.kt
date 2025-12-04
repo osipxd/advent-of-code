a45d926deee12dd4f43530e5bc7893da2d563f68
Osip Fatkullin <osip.fatkullin@gmail.com>
2025-12-04 11:50:18 +0100

    Day04: Part2 - Raw

--- advent-of-code-2025/src/Day04.kt
+++ advent-of-code-2025/src/Day04.kt
@@ -11,17 +11,30 @@ fun main() {
         measureAnswer { part1(input()) }
     }
 
-    //"Part 2" {
-    //    part2(testInput()) shouldBe 0
-    //    measureAnswer { part2(input()) }
-    //}
+    "Part 2" {
+        part2(testInput()) shouldBe 43
+        measureAnswer { part2(input()) }
+    }
 }
 
 private const val PAPER_ROLL = '@'
 
-private fun part1(input: Matrix<Char>): Int = input.valuePositions { it == PAPER_ROLL }.count { input.canBeAccessed(it) }
+private fun part1(input: Matrix<Char>): Int = input.valuePositions { it == PAPER_ROLL }.count(input::canBeAccessed)
 
-private fun part2(input: Matrix<Char>): Int = TODO()
+private fun part2(input: Matrix<Char>): Int {
+    var totalRemovedRolls = 0
+
+    var removedRolls = input.removeAccessibleRolls()
+    while (removedRolls > 0) {
+        totalRemovedRolls += removedRolls
+        removedRolls = input.removeAccessibleRolls()
+    }
+
+    return totalRemovedRolls
+}
+
+fun Matrix<Char>.removeAccessibleRolls(): Int =
+    valuePositions { it == PAPER_ROLL }.filter(this::canBeAccessed).onEach { this[it] = 'x' }.count()
 
 private fun Matrix<Char>.canBeAccessed(position: Position): Boolean =
     position.neighbors { this.getOrNull(it) != PAPER_ROLL }.count() > 4
